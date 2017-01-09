bcf5f720f4a29199332cefa7c50de8ef69e68480
alivemary <alivemary@gmail.com>
2017-01-09 10:03:58 +0100

    Choosing side added

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,4 +1,8 @@
 ﻿$(document).ready(function () {
+
+    drawX("#field10>canvas");
+    drawO("#field11>canvas");
+
     function drawX(where){
         var ctx = $(where)[0].getContext('2d');
         ctx.beginPath();
@@ -16,24 +20,28 @@
         ctx.arc(35, 35, 20, 0, 2 * Math.PI);
         ctx.stroke();
     }
+    function game(simbol) {
+        var otherSimbol = (simbol == 'X') ? "O" : "X";
+        $('h3').html("You: "+simbol+" Computer: "+otherSimbol);
+    }
     $('button').on('click', function () {
         switch (this.id) {
             case 'field10':
                 {
-                    drawX("#" + this.id + '>canvas');
                     $('#mainWin').removeClass('hidden');
                     $('#startWin').addClass('hidden');
                     $('#loadWin').addClass('hidden');
                     $('#failWin').addClass('hidden');
+                    game("X");
                     break;
                 }
             case 'field11':
                 {
-                    drawO("#" + this.id + '>canvas');
                     $('#mainWin').removeClass('hidden');
                     $('#startWin').addClass('hidden');
                     $('#loadWin').addClass('hidden');
                     $('#failWin').addClass('hidden');
+                    game("O");
                     break;
                 }
             default:
