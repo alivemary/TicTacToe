c60605ba035e72e37e4122204a6d4fdc6550e345
alivemary <alivemary@gmail.com>
2017-01-09 09:43:27 +0100

    0.1

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,5 +1,51 @@
 ﻿$(document).ready(function () {
-    $('#mainWin').removeClass('hidden');
+    function drawX(where){
+        var ctx = $(where)[0].getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(20, 50);
+        ctx.lineTo(50, 20);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(20, 20);
+        ctx.lineTo(50, 50);
+        ctx.stroke();
+    }
+    function drawO(where) {
+        var ctx = $(where)[0].getContext('2d');
+        ctx.beginPath();
+        ctx.arc(35, 35, 20, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+    $('button').on('click', function () {
+        switch (this.id) {
+            case 'field10':
+                {
+                    drawX("#" + this.id + '>canvas');
+                    $('#mainWin').removeClass('hidden');
+                    $('#startWin').addClass('hidden');
+                    $('#loadWin').addClass('hidden');
+                    $('#failWin').addClass('hidden');
+                    break;
+                }
+            case 'field11':
+                {
+                    drawO("#" + this.id + '>canvas');
+                    $('#mainWin').removeClass('hidden');
+                    $('#startWin').addClass('hidden');
+                    $('#loadWin').addClass('hidden');
+                    $('#failWin').addClass('hidden');
+                    break;
+                }
+            default:
+                {
+                    drawO("#" + this.id + '>canvas');
+                    break;
+                }
+       
+       }
+    });
+    $('#mainWin').addClass('hidden');
+    $('#startWin').removeClass('hidden');
     $('#loadWin').addClass('hidden');
     $('#failWin').addClass('hidden');
 });
