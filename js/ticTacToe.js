db6cd18455de14142982fb73feb003048c1902a2
alivemary <alivemary@gmail.com>
2017-01-13 18:21:44 +0100

    minimax works

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -26,19 +26,137 @@
     function game(simbol) {
         $('#mainWin').removeClass('hidden');
         $('#startWin').addClass('hidden');
-        var otherSimbol = (simbol == 'X') ? "O" : "X";
-        $('h3').html("You: " + simbol + " Computer: " + otherSimbol);
-        
-        function hasWon(player) {
-            var winCombo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-            for (var i = 0; i < winCombo.length; i++) {
-                
-                if (field[winCombo[i][0]] === player && field[winCombo[i][1]] === player && field[winCombo[i][2]] === player) {
-                    gameOver = true;
-                    $('h3').html(player + " won!");
-                    return true;
-                };
+
+        function Player(type, simbol) {
+            this.type = type;
+            this.simbol = simbol;
+            this.hasWon = function(field){
+                var winCombo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+                for (var i = 0; i < winCombo.length; i++) {
+                    if (field[winCombo[i][0]] === this.simbol && field[winCombo[i][1]] === this.simbol && field[winCombo[i][2]] === this.simbol) {
+                       
+                        return true;
+                    };
+                }
+                return false;
             }
+            this.checkVictory = function (field) {
+                if (this.hasWon(field)) {
+                    gameOver = true;
+                    $('h3').html(this.simbol + " won!");
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        var opponent, computer;
+
+        if (simbol == 'X') {
+            opponent = new Player("HUMAN", "X");
+            computer = new Player("COMPUTER", "O");
+        }
+        else {
+            computer = new Player("COMPUTER", "X");
+            opponent = new Player("HUMAN", "O");
+        }
+        
+        $('h3').html("You: " + opponent.simbol + " Computer: " + computer.simbol);
+
+        function calcLineScore(nFuild, line) {
+            var x = 0, o = 0, p = 0;
+            for (var i = 0; i < line.length; i++) {
+                switch (nFuild[line[i]]) {
+                    case "X":
+                        x++;
+                        break;
+                    case "O":
+                        o++;
+                        break;
+                    case " ":
+                        p++;
+                        break;
+                }
+            }
+            if (x === 3 && computer.simbol == "X") return 100;
+            if (o === 3 && computer.simbol == "O") return 100;
+            if (x === 3 && opponent.simbol == "X") return -100;
+            if (o === 3 && opponent.simbol == "O") return -100;
+            if (x === 2 && p === 1 && computer.simbol == "X") return 10;
+            if (o === 2 && p === 1 && computer.simbol == "O") return 10;
+            if (x === 2 && p === 1 && opponent.simbol == "X") return -10;
+            if (o === 2 && p === 1 && opponent.simbol == "O") return -10;
+            if (x === 1 && p === 2 && computer.simbol == "X") return 1;
+            if (o === 1 && p === 2 && computer.simbol == "O") return 1;
+            if (x === 1 && p === 2 && opponent.simbol == "X") return -1;
+            if (o === 1 && p === 2 && opponent.simbol == "O") return -1;
+            return 0;
+        }
+        function calcScore(nFuild) {
+            var winCombo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+            var score = 0;
+            winCombo.forEach(function (combo) {
+                score += calcLineScore(nFuild, combo);
+            });
+            return score;
+        }
+        //function is not ready yet
+       
+        function minimax(level, field, player, alpha, beta) {
+            var nFuild = field.slice();
+            var possibleMoves = [];
+            var score;
+            if (level === 0) return calcScore(nFuild);
+            if (player.hasWon(nFuild) || opponent.hasWon(nFuild)){
+                return calcScore(nFuild);
+            }
+            //All possible children
+            for (var i = 0; i < nFuild.length; i++) {
+                if (nFuild[i] == ' ') possibleMoves.push({ pos: i, score: undefined });
+            }
+            if (possibleMoves.length === 0) return 0;
+            //Computer turn
+            if (player.type == "COMPUTER") {
+                for (var i = 0; i < possibleMoves.length; i++) {
+                    nFuild[possibleMoves[i].pos] = player.simbol;
+                    possibleMoves[i].score = minimax(level-1, nFuild, opponent, alpha, beta);
+                    nFuild[possibleMoves[i].pos] = " ";
+                    if (possibleMoves[i].score > alpha) alpha = possibleMoves[i].score;
+                    if (alpha >= beta) break;
+                }
+                return alpha;
+            }
+            //Human turn
+            else {
+                for (var i = 0; i < possibleMoves.length; i++) {
+                    nFuild[possibleMoves[i].pos] = player.simbol;
+                    possibleMoves[i].score = minimax(level-1, nFuild, computer, alpha, beta);
+                    nFuild[possibleMoves[i].pos] = " ";
+                    if (possibleMoves[i].score < beta) beta = possibleMoves[i].score;
+                    if (alpha >= beta) break;
+                }
+                return beta;
+            }
+        }
+        function getMinMaxPos(field, player) {
+            var possibleMoves = [];
+            var nFuild = field.slice();
+            for (var i = 0; i < nFuild.length; i++) {
+                if (nFuild[i] == ' ') possibleMoves.push({ pos: i, score: undefined });
+
+            }
+            for (var i = 0; i < possibleMoves.length; i++) {
+                nFuild[possibleMoves[i].pos] = player.simbol;
+                possibleMoves[i].score = minimax(2, nFuild, opponent, -100000, 100000);
+                nFuild[possibleMoves[i].pos] = " ";
+            }
+            var max = 0;
+            for (var i = 1; i < possibleMoves.length;i++){
+                if (possibleMoves[i].score>possibleMoves[max].score){
+                    max = i;
+                }
+            }
+            return possibleMoves[max].pos;
         }
         $('button').on('click', function () {
             //human turn
@@ -47,27 +165,33 @@
             if (field.indexOf(' ') === -1 || gameOver || field[n] !== ' ') return;
             if (field.indexOf(' ') !== -1 && !gameOver) {
                 if (field[n] === ' ') {
-                    field[n] = simbol;
+                    field[n] = opponent.simbol;
                     console.log("#field" + n + ">canvas");
-                    if (simbol == 'X') drawX("#field" + (n+1) + ">canvas");
-                    if (simbol == 'O') drawO("#field" + (n + 1) + ">canvas");
+                    if (opponent.simbol == 'X') drawX("#field" + (n + 1) + ">canvas");
+                    if (opponent.simbol == 'O') drawO("#field" + (n + 1) + ">canvas");
                 }
                 console.log(field);
             }
-            else gameOver == true;
-            if (hasWon(simbol) || hasWon(otherSimbol)) return;
+            else {
+                gameOver == true;
+                
+            }
+            if (opponent.checkVictory(field)) return;
             //computer turn
             
             if (field.indexOf(' ') === -1 || gameOver) return;
             if (field.indexOf(' ') !== -1 && !gameOver) {
-                var m = field.indexOf(' ');
-                field[m] = otherSimbol;
-                if (simbol == 'X') drawO("#field" + (m + 1) + ">canvas");
-                if (simbol == 'O') drawX("#field" + (m + 1) + ">canvas");
+                var m = getMinMaxPos(field, computer);
+                field[m] = computer.simbol;
+                if (computer.simbol == 'X') drawX("#field" + (m + 1) + ">canvas");
+                if (computer.simbol == 'O') drawO("#field" + (m + 1) + ">canvas");
                 console.log(field);
             }
-            else gameOver == true;
-            if (hasWon(simbol) || hasWon(otherSimbol)) return;
+            else {
+                gameOver == true;
+                
+            }
+            if (computer.checkVictory(field)) return;
         });
         
     }
